5fbf6b48ebee18b2924eb078992f9146943c2463
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-11-29 10:00:07 -0300

    feat: lidar com erros globalmente e dar feedback visual

---
+++ src/utils/formatter.ts
@@ -0,0 +1,11 @@
+export function dateFormatter(isoDate: string): string {
+  const date = new Date(isoDate);
+  return new Intl.DateTimeFormat("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(date);
+}
