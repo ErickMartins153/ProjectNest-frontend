22c443bad6e65b8d3e92a52adecb0a1205240238
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-11-30 18:44:47 -0300

    feat: melhorar componentes de UI para uma melhor UX

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -13,9 +13,13 @@ export default function Input({ label, ...props }: InputProps) {
       <input
         {...props}
         placeholder=" "
-        className="peer w-full rounded-md border-2 p-2 pt-5 text-base focus:border-gray-300 focus:outline-none focus:ring-1"
+        id={`${label}-input`}
+        className="w-full p-2 pt-5 text-base border-2 rounded-md peer focus:border-gray-300 focus:outline-none focus:ring-1"
       />
-      <label className="absolute left-2 top-2 text-sm text-gray-500 transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:top-2 peer-focus:text-sm peer-focus:text-gray-500">
+      <label
+        htmlFor={`${label}-input`}
+        className="absolute text-sm text-gray-500 transition-all left-2 top-2 peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:top-2 peer-focus:text-sm peer-focus:text-gray-500"
+      >
         {label}
       </label>
     </div>
